4d813aa1fc5dc8844279c7cb0e820d99e65b0d0b
carlos jaramillo <cjalberto1503@gmail.com>
2024-07-26 16:29:31 -0400

    Add Fastlane configuration for automating deployment and testing

---
+++ Example/api/apiTests/apiTests.swift
@@ -0,0 +1,23 @@
+//
+//  apiTests.swift
+//  apiTests
+//
+//  Created by Carlos Jaramillo on 7/25/24.
+//
+
+import XCTest
+
+final class apiTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        XCTAssert(true, "succesfull")
+    }
+}
